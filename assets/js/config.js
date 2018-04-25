7b07259c54a8590e16d66c309cc5ac5319c1a53c
DyanWeng <34226228+DyanWeng@users.noreply.github.com>
2018-04-25 16:26:33 +0800

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+var android_url = "http://client.shlr01.com/app-124.apk";
+var ios_url = "itms-services://?action=download-manifest&url={{}}/hecbet.plist";
+var version = "";
---
+++ test.js
@@ -0,0 +1,4 @@
+var android_url = "http://client.tb068.com/app-77.apk"; 
+var ios_url = "itms-services://?action=download-manifest&url={{}}/hecbet.plist"; 
+var version = ""; 
+
